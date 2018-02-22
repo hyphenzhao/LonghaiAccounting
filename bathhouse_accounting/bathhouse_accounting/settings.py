9fcb8d9399eae8bc30aa99d0eeb89b8ac1a06134
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2018-02-22 20:41:02 +0800

    Update mysql users, fix bugs

--- bathhouse_accounting/bathhouse_accounting/settings.py
+++ bathhouse_accounting/bathhouse_accounting/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bathhouse',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
